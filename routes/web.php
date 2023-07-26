73db5094f183a7a7ad493659d4f01f9376b38df2
mzfa <zulfikarfebri1505@gmail.com>
2023-07-26 11:21:41 +0700

    akses routing

--- routes/web.php
+++ routes/web.php
@@ -33,98 +33,98 @@ use Maatwebsite\Excel\Facades\Excel;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
-Route::get('/login', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate'])->name('login.store');
-// Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'index'])->name('register');
-// Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'store'])->name('register.store');
-Route::post('/logout', function () {
-    auth()->logout();
-    request()->session()->invalidate();
-    request()->session()->regenerateToken();
+// Route::get('/', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
+// Route::get('/login', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
+// Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate'])->name('login.store');
+// // Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'index'])->name('register');
+// // Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'store'])->name('register.store');
+// Route::post('/logout', function () {
+//     auth()->logout();
+//     request()->session()->invalidate();
+//     request()->session()->regenerateToken();
     
-    return redirect('/');
-})->name('logout');
+//     return redirect('/');
+// })->name('logout');
 
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    // route::group()->middleware();
-    Route::controller(HakAksesController::class)->middleware('cek_login:hakakses.index')->group(function () {
-        Route::get('/hakakses', 'index')->name('hakakses.index');
-        Route::get('/hakakses/edit/{id}', 'edit');
-        Route::get('/hakakses/delete/{id}', 'delete');
-        Route::get('/hakakses/modul_akses/{id}', 'modul_akses');
-        Route::post('/hakakses/modul_akses', 'modul_akses_store');
-        Route::post('/hakakses/store', 'store');
-        Route::post('/hakakses/update', 'update');
-    });
-    Route::controller(MenuController::class)->middleware('cek_login:menu.index')->group(function () {
-        Route::get('/menu', 'index')->name('menu.index');
-        Route::get('/menu/edit/{id}', 'edit');
-        Route::get('/menu/status/{id}', 'status');
-        Route::get('/menu/delete/{id}', 'delete');
-        Route::post('/menu/store', 'store');
-        Route::post('/menu/update', 'update');
-    });
-    Route::controller(StrukturController::class)->middleware('cek_login:struktur.index')->group(function () {
-        Route::get('/struktur', 'index')->name('struktur.index');
-        Route::get('/struktur/edit/{id}', 'edit');
-        Route::get('/struktur/status/{id}', 'status');
-        Route::get('/struktur/delete/{id}', 'delete');
-        Route::post('/struktur/store', 'store');
-        Route::post('/struktur/update', 'update');
-    });
-    Route::controller(UserController::class)->middleware('cek_login:user.index')->group(function () {
-        Route::get('/user', 'index')->name('user.index');
-        Route::get('/user/sync', 'sync');
-        Route::get('/user/edit/{id}', 'edit');
-        Route::post('/user/update', 'update');
-    });
-    Route::controller(ListAkunController::class)->middleware('cek_login:struktur.index')->group(function () {
-        Route::get('/list_akun', 'index')->name('list_akun.index');
-        Route::get('/list_akun/edit/{id}', 'edit');
-        Route::get('/list_akun/qr/{id}', 'qr');
-        Route::get('/list_akun/status/{id}', 'status');
-        Route::get('/list_akun/delete/{id}', 'delete');
-        Route::post('/list_akun/store', 'store');
-        Route::post('/list_akun/update', 'update');
-    });
-    Route::controller(JenisSurveyController::class)->middleware('cek_login:jenis_survey.index')->group(function () {
-        Route::get('/jenis_survey', 'index')->name('jenis_survey.index');
-        Route::get('/jenis_survey/edit/{id}', 'edit');
-        Route::get('/jenis_survey/delete/{id}', 'delete');
-        Route::post('/jenis_survey/store', 'store');
-        Route::post('/jenis_survey/update', 'update');
-    });
-    Route::controller(KategoriSurveyController::class)->middleware('cek_login:kategori_survey.index')->group(function () {
-        Route::get('/kategori_survey', 'index')->name('kategori_survey.index');
-        Route::get('/kategori_survey/edit/{id}', 'edit');
-        Route::get('/kategori_survey/delete/{id}', 'delete');
-        Route::post('/kategori_survey/store', 'store');
-        Route::post('/kategori_survey/update', 'update');
-    });
-    Route::controller(PertanyaanController::class)->middleware('cek_login:pertanyaan.index')->group(function () {
-        Route::get('/pertanyaan', 'index')->name('pertanyaan.index');
-        Route::get('/pertanyaan/urutan', 'urutan');
-        Route::get('/pertanyaan/edit/{id}', 'edit');
-        Route::get('/pertanyaan/delete/{id}', 'delete');
-        Route::post('/pertanyaan/store', 'store');
-        Route::post('/pertanyaan/urutan', 'urutan_detail');
-        Route::post('/pertanyaan/urutan_detail', 'ubah_urutan');
-        Route::post('/pertanyaan/update', 'update');
-    });
-    Route::controller(LaporanSurveyController::class)->middleware('cek_login:laporan_survey.index')->group(function () {
-        Route::get('/laporan_survey/index', 'index')->name('laporan_survey.index');
-        Route::post('/laporan_survey/index', 'detail');
-        Route::get('/laporan_survey/export_excel/{awal}/{akhir}/{jenis_survey}/{kategori_survey}', 'export_excel');
-        // Route::post('/laporan_survey/detail', 'detail');
-    });
-    Route::controller(LaporanJenisPembayaranController::class)->middleware('cek_login:laporan_jenis_pembayaran.index')->group(function () {
-        Route::get('/laporan_jenis_pembayaran/index', 'index')->name('laporan_jenis_pembayaran.index');
-        Route::get('/laporan_jenis_pembayaran/export_excel/{jenis_pembayaran_id}/{tanggal_awal}/{tanggal_akhir}/{pemid}','export_excel');
-    });
-});
+// Route::group(['middleware' => ['auth']], function () {
+//     Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//     // route::group()->middleware();
+//     Route::controller(HakAksesController::class)->middleware('cek_login:hakakses.index')->group(function () {
+//         Route::get('/hakakses', 'index')->name('hakakses.index');
+//         Route::get('/hakakses/edit/{id}', 'edit');
+//         Route::get('/hakakses/delete/{id}', 'delete');
+//         Route::get('/hakakses/modul_akses/{id}', 'modul_akses');
+//         Route::post('/hakakses/modul_akses', 'modul_akses_store');
+//         Route::post('/hakakses/store', 'store');
+//         Route::post('/hakakses/update', 'update');
+//     });
+//     Route::controller(MenuController::class)->middleware('cek_login:menu.index')->group(function () {
+//         Route::get('/menu', 'index')->name('menu.index');
+//         Route::get('/menu/edit/{id}', 'edit');
+//         Route::get('/menu/status/{id}', 'status');
+//         Route::get('/menu/delete/{id}', 'delete');
+//         Route::post('/menu/store', 'store');
+//         Route::post('/menu/update', 'update');
+//     });
+//     Route::controller(StrukturController::class)->middleware('cek_login:struktur.index')->group(function () {
+//         Route::get('/struktur', 'index')->name('struktur.index');
+//         Route::get('/struktur/edit/{id}', 'edit');
+//         Route::get('/struktur/status/{id}', 'status');
+//         Route::get('/struktur/delete/{id}', 'delete');
+//         Route::post('/struktur/store', 'store');
+//         Route::post('/struktur/update', 'update');
+//     });
+//     Route::controller(UserController::class)->middleware('cek_login:user.index')->group(function () {
+//         Route::get('/user', 'index')->name('user.index');
+//         Route::get('/user/sync', 'sync');
+//         Route::get('/user/edit/{id}', 'edit');
+//         Route::post('/user/update', 'update');
+//     });
+//     Route::controller(ListAkunController::class)->middleware('cek_login:struktur.index')->group(function () {
+//         Route::get('/list_akun', 'index')->name('list_akun.index');
+//         Route::get('/list_akun/edit/{id}', 'edit');
+//         Route::get('/list_akun/qr/{id}', 'qr');
+//         Route::get('/list_akun/status/{id}', 'status');
+//         Route::get('/list_akun/delete/{id}', 'delete');
+//         Route::post('/list_akun/store', 'store');
+//         Route::post('/list_akun/update', 'update');
+//     });
+//     Route::controller(JenisSurveyController::class)->middleware('cek_login:jenis_survey.index')->group(function () {
+//         Route::get('/jenis_survey', 'index')->name('jenis_survey.index');
+//         Route::get('/jenis_survey/edit/{id}', 'edit');
+//         Route::get('/jenis_survey/delete/{id}', 'delete');
+//         Route::post('/jenis_survey/store', 'store');
+//         Route::post('/jenis_survey/update', 'update');
+//     });
+//     Route::controller(KategoriSurveyController::class)->middleware('cek_login:kategori_survey.index')->group(function () {
+//         Route::get('/kategori_survey', 'index')->name('kategori_survey.index');
+//         Route::get('/kategori_survey/edit/{id}', 'edit');
+//         Route::get('/kategori_survey/delete/{id}', 'delete');
+//         Route::post('/kategori_survey/store', 'store');
+//         Route::post('/kategori_survey/update', 'update');
+//     });
+//     Route::controller(PertanyaanController::class)->middleware('cek_login:pertanyaan.index')->group(function () {
+//         Route::get('/pertanyaan', 'index')->name('pertanyaan.index');
+//         Route::get('/pertanyaan/urutan', 'urutan');
+//         Route::get('/pertanyaan/edit/{id}', 'edit');
+//         Route::get('/pertanyaan/delete/{id}', 'delete');
+//         Route::post('/pertanyaan/store', 'store');
+//         Route::post('/pertanyaan/urutan', 'urutan_detail');
+//         Route::post('/pertanyaan/urutan_detail', 'ubah_urutan');
+//         Route::post('/pertanyaan/update', 'update');
+//     });
+//     Route::controller(LaporanSurveyController::class)->middleware('cek_login:laporan_survey.index')->group(function () {
+//         Route::get('/laporan_survey/index', 'index')->name('laporan_survey.index');
+//         Route::post('/laporan_survey/index', 'detail');
+//         Route::get('/laporan_survey/export_excel/{awal}/{akhir}/{jenis_survey}/{kategori_survey}', 'export_excel');
+//         // Route::post('/laporan_survey/detail', 'detail');
+//     });
+//     Route::controller(LaporanJenisPembayaranController::class)->middleware('cek_login:laporan_jenis_pembayaran.index')->group(function () {
+//         Route::get('/laporan_jenis_pembayaran/index', 'index')->name('laporan_jenis_pembayaran.index');
+//         Route::get('/laporan_jenis_pembayaran/export_excel/{jenis_pembayaran_id}/{tanggal_awal}/{tanggal_akhir}/{pemid}','export_excel');
+//     });
+// });
 
 
 Route::controller(SurveyEksekutifController::class)->group(function () {
